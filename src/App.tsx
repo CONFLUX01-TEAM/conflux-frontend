d9ef83bb6af79a840eb9ab07992ca4552224e6bd
MAYOR-001-star <mayorkunabdulazeez01@gmail.com>
2026-06-16 12:01:05 +0100

    feat: implement onboarding steps, validation, and dynamic layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import MainLayout from './layouts/MainLayout';
 import AuthLayout from './layouts/AuthLayout';
 import Auth from './pages/Auth/Auth';
 import VerifyEmail from './pages/Auth/VerifyEmail';
+import OnboardingPage from './pages/onboarding/Onboarding';
+import OnboardingLayout from './layouts/OnboardingLayout';
 // import Home from './pages/Home';
 
 const App = () => {
@@ -16,6 +18,10 @@ const App = () => {
         <Route path="signin" element={<Auth authState='signin' />} />
         <Route path="signup" element={<Auth authState='signup' />} />
         <Route path="verify-email" element={<VerifyEmail />} />
+      </Route>
+
+      <Route element={<OnboardingLayout />}>
+        <Route path="onboarding" element={<OnboardingPage />} />
       </Route>
     </Routes>
   );
